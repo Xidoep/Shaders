d940a2f1f6db8205a6c7d6adbbccb5711bc38c7e
Xidostudio <45095517+Xidoep@users.noreply.github.com>
2022-11-07 22:37:00 +0100

    CAnvis per adaptar a joc

--- Pel/Scripts/Pel.cs
+++ Pel/Scripts/Pel.cs
@@ -3,59 +3,54 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-    public class Pel : MonoBehaviour
+public class Pel : MonoBehaviour
+{
+    const string KEY_ALTURA = "_altura";
+    const string KEY_OFFSET = "_offset";
+
+    Camera cam;
+    [SerializeField] MeshFilter meshFilter;
+    [SerializeField] MeshRenderer meshRenderer;
+
+    [Range(0, 31)] [SerializeField] int horizontalStackSize = 20;
+    [Range(0.01f, 20)] [SerializeField] float altura = 3;
+    [SerializeField] bool sombra;
+
+    Matrix4x4[] matrix;
+    MaterialPropertyBlock mpb;
+
+
+
+    private void Start() => Init();
+    private void Init() 
     {
-        const string KEY_CAPA = "_capa";
-        const string KEY_OFFSET = "_offset";
-
-        Camera cam;
-
-        public MeshFilter meshFilter;
-        public MeshRenderer meshRenderer;
-
-        [Range(0, 31)] public int horizontalStackSize = 20;
-        [Range(0.01f, 20)] public float altura = 3;
-        public bool sombra;
-
-        //float offset;
-        //int layer = 1;
-
-        private Matrix4x4 matrix;
-        MaterialPropertyBlock mpb;
+        cam = Camera.main;
+    } 
 
 
-        private void Awake() 
+    private void OnEnable()
+    {
+        mpb = new MaterialPropertyBlock();
+        if (meshFilter == null) meshFilter.GetComponent<MeshFilter>();
+        if (meshRenderer == null) meshRenderer.GetComponent<MeshRenderer>();
+
+        matrix = new Matrix4x4[1];
+
+        if (cam == null)
+            Init();
+    }
+
+    void LateUpdate()
+    {
+        matrix[0] = Matrix4x4.TRS(transform.position, transform.rotation, transform.localScale);
+
+        for (int i = 0; i < horizontalStackSize; i++)
         {
-            mpb = new MaterialPropertyBlock();
-            if (meshFilter == null) meshFilter.GetComponent<MeshFilter>();
-            if (meshRenderer == null) meshRenderer.GetComponent<MeshRenderer>();
-        } 
-        private void Start() => Init();
-        private void Init() => cam = Camera.main;
-
-
-        private void OnEnable()
-        {
-            if (meshFilter == null) meshFilter.GetComponent<MeshFilter>();
-            if (meshRenderer == null) meshRenderer.GetComponent<MeshRenderer>();
-        }
-
-        void Update()
-        {
-            if (cam == null)
-                Init();
-
-            matrix = Matrix4x4.TRS(transform.position, transform.rotation, transform.localScale);
-
-            if (mpb == null) mpb = new MaterialPropertyBlock();
-
-            for (int i = 0; i < horizontalStackSize; i++)
-            {
-                mpb.SetFloat("_altura",  altura);
-                mpb.SetFloat(KEY_OFFSET, altura - ((altura / (float)horizontalStackSize) * i));
-                Graphics.DrawMesh(meshFilter.mesh, matrix, meshRenderer.material, 1, cam, 0, mpb, sombra, false, false);
-            }
+            mpb.SetFloat(KEY_ALTURA,  altura);
+            mpb.SetFloat(KEY_OFFSET, altura - ((altura / (float)horizontalStackSize) * i));
+            Graphics.DrawMeshInstanced(meshFilter.mesh, 0, meshRenderer.material, matrix, 1, mpb);
         }
     }
+}
 
 
